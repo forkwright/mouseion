77709a8516cae9c7d66f393c350e1f532356e39e
admin <admin@ardentleatherworks.com>
2026-01-03 14:19:23 -0600

    fix: add full copyright headers to new files

--- src/Mouseion.Core/Filtering/FilterQueryBuilder.cs
+++ src/Mouseion.Core/Filtering/FilterQueryBuilder.cs
@@ -1,6 +1,12 @@
 // Copyright (c) 2025 Mouseion Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+// Mouseion - Unified media manager
+// Copyright (C) 2024-2025 Mouseion Contributors
+// Based on Radarr (https://github.com/Radarr/Radarr)
+// Copyright (C) 2010-2025 Radarr Contributors
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 using Dapper;
 
 namespace Mouseion.Core.Filtering;
