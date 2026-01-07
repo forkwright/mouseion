1eaf6e7aeaab2b657c8bd6eb0445b70777e3194b
admin <admin@ardentleatherworks.com>
2026-01-07 13:05:02 -0600

    refactor(api): extract BuildCalendarEntries helper to reduce duplication

--- src/Mouseion.Core/Movies/Calendar/MovieCalendarService.cs
+++ src/Mouseion.Core/Movies/Calendar/MovieCalendarService.cs
@@ -46,56 +46,7 @@ public class MovieCalendarService : IMovieCalendarService
             }
         }
 
-        var entries = new List<MovieCalendarEntry>();
-
-        foreach (var movie in movies)
-        {
-            var hasFile = moviesWithFiles.Contains(movie.Id);
-
-            if (movie.InCinemas.HasValue && movie.InCinemas.Value >= start && movie.InCinemas.Value <= end)
-            {
-                entries.Add(new MovieCalendarEntry
-                {
-                    MovieId = movie.Id,
-                    Title = movie.Title,
-                    Year = movie.Year,
-                    ReleaseDate = movie.InCinemas.Value,
-                    ReleaseType = "Cinema",
-                    Monitored = movie.Monitored,
-                    HasFile = hasFile
-                });
-            }
-
-            if (movie.DigitalRelease.HasValue && movie.DigitalRelease.Value >= start && movie.DigitalRelease.Value <= end)
-            {
-                entries.Add(new MovieCalendarEntry
-                {
-                    MovieId = movie.Id,
-                    Title = movie.Title,
-                    Year = movie.Year,
-                    ReleaseDate = movie.DigitalRelease.Value,
-                    ReleaseType = "Digital",
-                    Monitored = movie.Monitored,
-                    HasFile = hasFile
-                });
-            }
-
-            if (movie.PhysicalRelease.HasValue && movie.PhysicalRelease.Value >= start && movie.PhysicalRelease.Value <= end)
-            {
-                entries.Add(new MovieCalendarEntry
-                {
-                    MovieId = movie.Id,
-                    Title = movie.Title,
-                    Year = movie.Year,
-                    ReleaseDate = movie.PhysicalRelease.Value,
-                    ReleaseType = "Physical",
-                    Monitored = movie.Monitored,
-                    HasFile = hasFile
-                });
-            }
-        }
-
-        return entries.OrderBy(e => e.ReleaseDate).ToList();
+        return BuildCalendarEntries(movies, start, end, moviesWithFiles);
     }
 
     public List<MovieCalendarEntry> GetCalendarEntries(DateTime start, DateTime end, bool includeUnmonitored = false)
@@ -117,6 +68,15 @@ public class MovieCalendarService : IMovieCalendarService
             }
         }
 
+        return BuildCalendarEntries(movies, start, end, moviesWithFiles);
+    }
+
+    private List<MovieCalendarEntry> BuildCalendarEntries(
+        IEnumerable<Movie> movies,
+        DateTime start,
+        DateTime end,
+        HashSet<int> moviesWithFiles)
+    {
         var entries = new List<MovieCalendarEntry>();
 
         foreach (var movie in movies)
