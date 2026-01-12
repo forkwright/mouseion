2b7901b77659953bc87ba520a5411fa593705051
admin <admin@ardentleatherworks.com>
2026-01-12 11:58:35 -0600

    test: add integration tests for GetById and Delete with real items

--- tests/Mouseion.Api.Tests/MediaItems/MediaItemsControllerTests.cs
+++ tests/Mouseion.Api.Tests/MediaItems/MediaItemsControllerTests.cs
@@ -3,8 +3,11 @@
 
 using System.Net;
 using System.Net.Http.Json;
+using Mouseion.Api.Authors;
+using Mouseion.Api.Books;
 using Mouseion.Api.Common;
 using Mouseion.Api.MediaItems;
+using Mouseion.Api.Movies;
 
 namespace Mouseion.Api.Tests.MediaItems;
 
@@ -156,5 +159,100 @@ public class MediaItemsControllerTests : ControllerTestBase
 
         var result = await response.Content.ReadFromJsonAsync<List<MediaItemResource>>();
         Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task GetById_WithExistingBook_ReturnsMediaItemDetail()
+    {
+        var author = await CreateAuthor("Test Author");
+        var book = await CreateBook("Test Book", 2024, author.Id);
+
+        var response = await Client.GetAsync($"/api/v3/media/{book.Id}");
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<MediaItemDetailResource>();
+        Assert.NotNull(result);
+        Assert.Equal(book.Id, result.Id);
+        Assert.Equal("Test Book", result.Title);
+    }
+
+    [Fact]
+    public async Task GetById_WithExistingMovie_ReturnsMediaItemDetail()
+    {
+        var movie = await CreateMovie("Test Movie", 2024);
+
+        var response = await Client.GetAsync($"/api/v3/media/{movie.Id}");
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<MediaItemDetailResource>();
+        Assert.NotNull(result);
+        Assert.Equal("Test Movie", result.Title);
+    }
+
+    [Fact]
+    public async Task Delete_WithExistingItem_ReturnsNoContent()
+    {
+        var author = await CreateAuthor("Author to Delete");
+        var book = await CreateBook("Book to Delete", 2024, author.Id);
+
+        var response = await Client.DeleteAsync($"/api/v3/media/{book.Id}");
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+        var getResponse = await Client.GetAsync($"/api/v3/media/{book.Id}");
+        Assert.Equal(HttpStatusCode.NotFound, getResponse.StatusCode);
+    }
+
+    private async Task<AuthorResource> CreateAuthor(string name)
+    {
+        var author = new AuthorResource
+        {
+            Name = name,
+            Monitored = true,
+            QualityProfileId = 1
+        };
+
+        var response = await Client.PostAsJsonAsync("/api/v3/authors", author);
+        response.EnsureSuccessStatusCode();
+
+        var created = await response.Content.ReadFromJsonAsync<AuthorResource>();
+        Assert.NotNull(created);
+        return created;
+    }
+
+    private async Task<BookResource> CreateBook(string title, int year, int authorId)
+    {
+        var book = new BookResource
+        {
+            Title = title,
+            Year = year,
+            Monitored = true,
+            QualityProfileId = 1,
+            AuthorId = authorId
+        };
+
+        var response = await Client.PostAsJsonAsync("/api/v3/books", book);
+        response.EnsureSuccessStatusCode();
+
+        var created = await response.Content.ReadFromJsonAsync<BookResource>();
+        Assert.NotNull(created);
+        return created;
+    }
+
+    private async Task<MovieResource> CreateMovie(string title, int year)
+    {
+        var movie = new MovieResource
+        {
+            Title = title,
+            Year = year,
+            Monitored = true,
+            QualityProfileId = 1
+        };
+
+        var response = await Client.PostAsJsonAsync("/api/v3/movies", movie);
+        response.EnsureSuccessStatusCode();
+
+        var created = await response.Content.ReadFromJsonAsync<MovieResource>();
+        Assert.NotNull(created);
+        return created;
     }
 }
